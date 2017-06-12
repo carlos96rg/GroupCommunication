ab91ab362eca0f1c80a0c09224c099e536fe19e0
Carlos Rincon <carlos.rincon@estudiants.urv.cat>
2017-06-12 13:52:35 +0200

    Tests fixed

--- test/group-test.py
+++ test/group-test.py
@@ -13,19 +13,22 @@ class GroupTest(unittest.TestCase):
     def test_group(self):
         group = self.host.spawn('group', Group)
         identifier = group.join('1')
+        group.init_start()
         self.host2 = create_host('http://127.0.0.1:1282')
         peer = self.host2.spawn('peer', Peer)
 
         self.assertEqual(0, identifier)
 
-        sleep(21)
+        sleep(26)
         self.assertEqual([], group.get_members())
 
         identifier2 = group.join(peer.get_url())
         self.assertEqual(identifier2, 1)
         group.join('2')
         group.join('3')
-        group.leave('2', 1)
+        print group.get_members()
+        group.leave('2', 2)
+        self.assertEqual([('3', 3), ('http://127.0.0.1:1282/peer', 1)], group.get_members())
 
     def tearDown(self):
         shutdown()
